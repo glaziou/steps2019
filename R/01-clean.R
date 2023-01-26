2cf041afaba24b77a14aa8fe973e7e09ecb975c8
Philippe GLAZIOU <glaziou@gmail.com>
2023-01-25 15:23:34 -1000

    pop and sampling weights

--- R/01-clean.R
+++ R/01-clean.R
@@ -19,12 +19,15 @@
 #'
 library(data.table)
 library(here)
+library(haven)
 
 
 #' load data
 #' 
 steps <- fread(here('input/PF_STEPS2019_V3_2_1_results.csv')) # STEP 1
 loc <- fread(here('input/loc.csv'))  # geo codes
+pop <- as.data.table(read_dta(here('input/ponderation_draft.dta')))
+
 
 
 #' clean-up locations
@@ -57,6 +60,8 @@ steps[, table(`step1:demographics:agerange`)]
 steps[, table(`step1:demographics:agerange`, `step1:demographics:age`)] # looks OK
 steps[, age := `step1:demographics:age`]
 steps[, agegroup := `step1:demographics:agerange`]
+steps[, agegr := agegroup]
+steps[agegr %in% c('45-59', '60-69'), agegr := '45-69']
 
 # 9 participants aged 70 (not eligible)
 steps[age==70, .(codeloc, commune, age, agegroup)]
@@ -84,12 +89,43 @@ steps[agegroup=='', eligible := FALSE]
 steps[, .N]
 steps[, .N, by=eligible] # age documented and in (18-69)
 steps[eligible==F, table(archipel)]
-steps[eligible==T, table(agegroup, archipel)]
+steps[eligible==T, table(agegr, archipel)]
 
 
 
+#' Include pop and calculate weights
+#' 
+s <- copy(steps)
+steps[agegr=='18-29', agecat := 1]
+steps[agegr=='30-44', agecat := 2]
+steps[agegr=='45-69', agecat := 3]
+steps[, sex := `step1:demographics:C1`]
+steps[eligible==TRUE, table(sex)]
+steps[eligible==TRUE, table(sex, agecat)]
+
+pop[archipelago %in% c('Australes','Marquises'), archipel := archipelago]
+pop[archipelago=='Société (Îles-du-vent)', archipel := 'IDV']
+pop[archipelago=='Société (Îles-sous-le-vent)', archipel := 'ISLV']
+pop[archipelago=='Tuamotu-Gambier', archipel := 'Tuamotu']
+pop[, sex := gender]
+
+steps <- merge(steps[eligible==TRUE], pop[,.(archipel, sex, agecat=age_cat, pop=N)], by=c('archipel','sex','agecat'), all.x=TRUE)
+steps[, .(archipel, sex, agecat, pop)]
 
 
+# 3 geo strata
+steps[, gstratum := archipel]
+steps[archipel %in% c('Australes','Marquises','Tuamotu'), gstratum := 'Autres']
 
+
+# sample size by stratum
+(out <- steps[, .N, by=.(archipel, sex, agecat, pop)])
+# 3 geo strata
+out[, gstratum := archipel]
+out[archipel %in% c('Australes','Marquises','Tuamotu'), gstratum := 'Autres']
+out <- out[, .(N=sum(N), pop=sum(pop)), by=.(gstratum, sex, agecat)]
+out[, f := N/pop]  # sampling fraction
+out[, w := 1/f]    # sampling weights
+(out)
 
 
